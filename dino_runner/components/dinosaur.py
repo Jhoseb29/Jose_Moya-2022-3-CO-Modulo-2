4c1147dbcec6fa2affb35fedc6eb5806c4576149
Jhoseb <jhoseb29@gmail.com>
2022-11-07 20:29:29 -0500

    Solucion tarea agachar al dino

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -9,6 +9,7 @@ class Dinosaur(Sprite):
     y_pos = 310
     JUMP_SPEED = 6
 
+
     def __init__(self):
         self.image = RUNNING[0]
         self.rect = self.image.get_rect()
@@ -18,18 +19,28 @@ class Dinosaur(Sprite):
         self.dino_run = True
         self.jump_speed = self.JUMP_SPEED
         self.dino_jump = False
+        self.dino_duck = False
+        
     def update(self, user_input):
         if self.dino_run:
             self.run()
         elif self.dino_jump:
             self.jump()
         
+        elif self.dino_duck:
+            self.duck()
+
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
         elif not self.dino_jump:
             self.dino_jump = False
             self.dino_run = True
+
+        if user_input[pygame.K_DOWN]:
+            self.dino_duck = True
+            self.dino_run = False
+            
 
         if self.step_index >= 10:
             self.step_index = 0
@@ -52,7 +63,14 @@ class Dinosaur(Sprite):
             self.dino_jump = False
             self.jump_speed = self.JUMP_SPEED
 
+    def duck(self):
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x_pos
+        self.rect.y = self.y_pos + 30
+        self.step_index += 1
     
+
 
     def draw(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
